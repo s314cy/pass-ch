ba09292e37c4fac7a9bee46685cddd3f35d91c3d
sev <hugo.elguedj@epfl.ch>
2020-08-01 00:29:24 +0200

    README update, grade display

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#CH Pass
+# CH Pass
 
 Example usage:
 ```python pass.py --marks 88/100 79/100 --weights 0.15/0.7 0.15/0.7 --target 57.5/100``` 
--- pass.py
+++ pass.py
@@ -29,5 +29,5 @@ if __name__ == '__main__':
 
     points = sum([mark * weight for mark, weight in zip(marks, weights)])
     final_weight = 1 - sum(weights)
-    final_score = abs(target - points) / final_weight * 100
-    print(f'Need to score {final_score} points on final to reach {target * 100}')
+    final_score = abs(target - points) / final_weight
+    print(f'Need to score {final_score * 100}/100 points on final to reach {target * 5 + 1}')
