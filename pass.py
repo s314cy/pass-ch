df1e9295a1c07da8bfe7ee504483e8909f37e421
sev <hugo.elguedj@epfl.ch>
2020-08-01 18:20:01 +0200

    Score rounding, better display

--- pass.py
+++ pass.py
@@ -1,4 +1,5 @@
 import argparse
+import math
 
 
 def string_to_float(quotient):
@@ -27,4 +28,7 @@ if __name__ == '__main__':
     points = sum([mark * weight for mark, weight in zip(marks, weights)])
     final_weight = 1 - sum(weights)
     final_score = abs(target - points) / final_weight
-    print(f'Need to score {final_score * 100}/100 points on final to reach {target * 5 + 1}')
+    
+    display_score = math.ceil(final_score * 1000) / 10
+    display_target = target * 5 + 1
+    print(f'Need to score {display_score:.1f}/100 points on final to reach {display_target:.3f}')
