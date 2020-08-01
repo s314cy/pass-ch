a7ae46dffcb17fdbd54ac2d675b447fb480f50ef
sev <hugo.elguedj@epfl.ch>
2020-08-01 10:25:47 +0200

    Remove useless for-loop

--- credits.py
+++ credits.py
@@ -10,10 +10,7 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--credits', type=int, nargs='+')
     args = parser.parse_args()
 
-    possibilities = []
     for r in range(1, len(args.credits) + 1):
         for c in combinations(args.credits, r):
-            possibilities.append(c)
-    for p in possibilities:
-        if args.base + sum(p) >= args.goal:
-            print(*p)
+            if args.base + sum(c) >= args.goal:
+                print(*c)
